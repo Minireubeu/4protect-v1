2f936d126d0296ff4be27e653ea76699f729ac67
Minireubeu <xnam697@gmail.com>
2025-05-05 19:09:35 +0200

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        buyer: '826729053615816724', // Buyer
+        couleur: '#ff69b4', // Couleur HEX
+        footer: '🌸Yaskoo Leak & Replace 🌸', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
